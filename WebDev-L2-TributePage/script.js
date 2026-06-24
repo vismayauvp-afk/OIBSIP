4cd99f393ab8ed338762e73a9649ba3628e2697a
vismayauvp-afk <vismayauvp@gmail.com>
2026-06-24 22:26:36 +0530

    Added README for Calculator

---
+++ WebDev-L2-TributePage/script.js
@@ -0,0 +1,13 @@
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, {
+    threshold: 0.15
+});
+
+sections.forEach(sec => observer.observe(sec));
